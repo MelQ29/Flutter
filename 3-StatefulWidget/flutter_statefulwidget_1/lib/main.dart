013498b508fb0bbc69f46f5952dcdec7797e500f
Vladislav Melnikov <MelQQ29@yandex.ru>
2023-01-18 00:03:19 +0200

    Introductuin to StatefulWidgets

--- 3-StatefulWidget/flutter_statefulwidget_1/lib/main.dart
+++ 3-StatefulWidget/flutter_statefulwidget_1/lib/main.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyFirstApp());
@@ -13,6 +15,18 @@ class MyFirstApp extends StatefulWidget {
 }
 
 class _MyFirstAppState extends State<MyFirstApp> {
+  // ignore: unused_field
+  late bool _loading;
+  // ignore: unused_field
+  late double _progressValues;
+
+  @override
+  void initState() {
+    _loading = false;
+    _progressValues = 0.0;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,26 +40,50 @@ class _MyFirstAppState extends State<MyFirstApp> {
           // ignore: avoid_unnecessary_containers
           child: Container(
             padding: EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: <Widget>[
-                LinearProgressIndicator(value: 69),
-                Text(
-                  '69 %',
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                ),
-                Text(
-                  'Press button to download',
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                ),
-              ],
-            ),
+            child: _loading
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: <Widget>[
+                      LinearProgressIndicator(value: _progressValues),
+                      Text(
+                        '${(_progressValues * 100).round()}%',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ],
+                  )
+                : Text(
+                    'Press button to download',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
           ),
         ),
         floatingActionButton: FloatingActionButton(
-            onPressed: null, child: Icon(Icons.cloud_download)),
+            onPressed: () {
+              setState(() {
+                _loading = !_loading;
+                _updateProgress();
+              });
+            },
+            child: Icon(Icons.cloud_download)),
       ),
     );
+  }
+
+  void _updateProgress() {
+    // ignore: unnecessary_const
+    const oneSec = const Duration(seconds: 1);
+    Timer.periodic(oneSec, (Timer t) {
+      setState(() {
+        _progressValues += 0.2;
+
+        if (_progressValues.toStringAsFixed(1) == '1.0') {
+          _loading = false;
+          t.cancel();
+          _progressValues = 0.0;
+          return;
+        }
+      });
+    });
   }
 }
